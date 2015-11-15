67eb5587c448669ff9030425b4e38cda16052a53
houcharlie <ch22@princeton.edu>
2015-11-15 02:09:03 -0500

    Update MusicListener.java

--- MusicListener.java
+++ MusicListener.java
@@ -4,95 +4,98 @@ import com.leapmotion.leap.*;
 import com.leapmotion.leap.Gesture.State;
 
 class MusicListener extends Listener {
+
 	private double speed = 0;
 	private double volume = 0;
+	// unit amount speed changes per gesture
 	private final double DELTA_S = .004;
+	// unit amount volume changes per gesture
 	private final double DELTA_V = .7;
 	private boolean tap = false;
 
 	public void onInit(Controller controller) {
-        System.out.println("Initialized");
-    }
+		System.out.println("Initialized");
+	}
 
-    public void onConnect(Controller controller) {
-        System.out.println("Connected");
-        controller.enableGesture(Gesture.Type.TYPE_SWIPE);
-        controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
-        controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
-        controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
-    }
-    public double getTempo() 
-    {
-    	return speed;
-    }
-    public double getVolume()
-    {
-    	return volume;
-    }
-    public boolean getTap()
-    {
-    	return tap;
-    }
-    public void onDisconnect(Controller controller) {
-        //Note: not dispatched when running in a debugger.
-        System.out.println("Disconnected");
-    }
+	public void onConnect(Controller controller) {
+		System.out.println("Connected");
+		controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+		controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+		controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+		controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+	}
 
-    public void onExit(Controller controller) {
-        System.out.println("Exited");
-    }
-    
-    public void onFrame(Controller controller) {
-        // Get the most recent frame and report some basic information
-        Frame frame = controller.frame();
+	public double getTempo() {
+		return speed;
+	}
 
-        GestureList gestures = frame.gestures();
-        for (int i = 0; i < gestures.count(); i++) {
-            Gesture gesture = gestures.get(i);
+	public double getVolume() {
+		return volume;
+	}
 
-            switch (gesture.type()) {
-                case TYPE_CIRCLE:
-                    CircleGesture circle = new CircleGesture(gesture);
+	public boolean getTap() {
+		return tap;
+	}
 
-                    // Calculate clock direction using the angle between circle normal and pointable
+	public void onDisconnect(Controller controller) {
+		// Note: not dispatched when running in a debugger.
+		System.out.println("Disconnected");
+	}
 
-                    if (circle.pointable().direction().angleTo(circle.normal()) <= Math.PI/2) {
-                        // Clockwise if angle is less than 90 degrees
-                        speed += DELTA_S;
-                    } else {
-                        speed -= DELTA_S;
-                    }
+	public void onExit(Controller controller) {
+		System.out.println("Exited");
+	}
 
-                    // Calculate angle swept since last frame
-                    double sweptAngle = 0;
-                    if (circle.state() != State.STATE_START) {
-                        CircleGesture previousUpdate = new CircleGesture(controller.frame(1).gesture(circle.id()));
-                        sweptAngle = (circle.progress() - previousUpdate.progress()) * 2 * Math.PI;
-                    }
+	public void onFrame(Controller controller) {
+		// Get the most recent frame and report some basic information
+		Frame frame = controller.frame();
 
-                    break;
-                case TYPE_SWIPE:
-                    SwipeGesture swipe = new SwipeGesture(gesture);
-                    if (swipe.pointable().direction().getX() > 0)
-                    {
-                    	volume += DELTA_V;
-                    }
-                    else
-                    {
-                    	volume -= DELTA_V;
-                    }
-                    
-                    break;
-                
-                case TYPE_SCREEN_TAP:
-                	ScreenTapGesture tapGesture = new ScreenTapGesture(gesture);
-                	tap = true;
-                	
-                default:
-                    break;
-            }
-        }
+		GestureList gestures = frame.gestures();
+		for (int i = 0; i < gestures.count(); i++) {
+			Gesture gesture = gestures.get(i);
 
-    }
+			switch (gesture.type()) {
+			case TYPE_CIRCLE:
+				CircleGesture circle = new CircleGesture(gesture);
+
+				// Calculate clock direction using the angle between circle
+				// normal and pointable
+
+				if (circle.pointable().direction().angleTo(circle.normal()) <= Math.PI / 2) {
+					// Clockwise if angle is less than 90 degrees
+					speed += DELTA_S;
+				} else {
+					speed -= DELTA_S;
+				}
+
+				// Calculate angle swept since last frame
+				double sweptAngle = 0;
+				if (circle.state() != State.STATE_START) {
+					CircleGesture previousUpdate = new CircleGesture(controller
+							.frame(1).gesture(circle.id()));
+					sweptAngle = (circle.progress() - previousUpdate.progress())
+							* 2 * Math.PI;
+				}
+
+				break;
+			case TYPE_SWIPE:
+				SwipeGesture swipe = new SwipeGesture(gesture);
+				if (swipe.pointable().direction().getX() > 0) {
+					volume += DELTA_V;
+				} else {
+					volume -= DELTA_V;
+				}
+
+				break;
+
+			case TYPE_SCREEN_TAP:
+				ScreenTapGesture tapGesture = new ScreenTapGesture(gesture);
+				tap = true;
+
+			default:
+				break;
+			}
+		}
+
+	}
 }
-
